14e64ac170c399127b55045ab820b9451ec2b632
Dempsey Blum <dempsey@curiousit.com>
2023-08-29 06:37:42 +1000

    Add army list queries

---
+++ frontend/queries/armyListQueries.ts
@@ -0,0 +1,54 @@
+import { gql } from "@apollo/client";
+
+interface ArmyList {
+  id: string;
+  attributes: {
+    name: string;
+  };
+}
+
+export interface ArmyListsReturnType {
+  armyLists: {
+    data: ArmyList[];
+  };
+}
+
+export interface SingleArmyListReturnType {
+  armyList: {
+    data: ArmyList;
+  };
+}
+
+export const armyListByIDQuery = gql`
+  query GetArmyListByID($armyListID: ID!) {
+    coreKeywords(id: $armyListID) {
+      data {
+        id
+        attributes {
+          name
+        }
+      }
+    }
+  }
+`;
+
+// Need to check how to actually filter on users
+export const armyListByUserQuery = gql`
+  query GetArmyListByUser($userID: ID!) {
+    coreKeywords(filter: { user: { id: { eq: $userID } } })
+    data {
+      id
+      attributes {
+        name
+        user {
+          data {
+            id
+            attributes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
